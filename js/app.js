7560490e4c38c589b434496ad7347215ccbc2e48
DylanSDev <dylansdiaz.dev@gmail.com>
2023-11-28 11:49:03 -0300

    Se ha desarrollado la función para adivinar el número

--- js/app.js
+++ js/app.js
@@ -6,4 +6,28 @@ const generarNumero = () => {
   alert("¡Se ha generado el número mágico!");
   const primeraPantalla = document.getElementById("genNumero");
   primeraPantalla.style.display = "none";
+  const segundaPantalla = document.getElementById("contenedorJuego");
+  segundaPantalla.style.display = "block";
 };
+
+const enviarNumero = () => {
+  const numeroIngresado = parseInt(
+    document.getElementById("inputNumero").value
+  );
+  document.getElementById("inputNumero").value = "";
+
+  if (isNaN(numeroIngresado)) {
+    alert("Ingresé un número valido");
+  } else {
+    if (numeroIngresado == numeroMagico) {
+      alert("¡HAZ ADIVINADO EL NÚMERO MÁGICO!");
+    }
+    if (numeroIngresado > numeroMagico)
+      alert("El número ingresado es mayor al número mágico");
+    if (numeroIngresado < numeroMagico)
+      alert("El número ingresado es menor al número mágico");
+  }
+};
+
+const btnEnviarNumero = document.getElementById("btnEnviarNumero");
+btnEnviarNumero.addEventListener("click", enviarNumero);
